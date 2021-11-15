a3b740d8f93117ae6c823aabb67c017055cab310
m_bubela <m_bubela@mifprojects.com>
2021-11-15 13:21:35 +0200

    Added user registr model

---
+++ TokenStady/TokenStady/Models/UserRegistrationModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MintyIssueTracker.Models
+{
+    public class UserRegistrationModel
+    {
+        [Required, MinLength(4), MaxLength(16)]
+        public string Username { get; set; }
+
+        [Required, MinLength(6), MaxLength(16)]
+        public string Password { get; set; }
+
+        [Required, MinLength(6), MaxLength(16), Compare("Password")]
+        public string RePassword { get; set; }
+
+        [Required, MinLength(3), MaxLength(16)]
+        public string Firstname { get; set; }
+
+        [Required, MinLength(3), MaxLength(16)]
+        public string Lastname { get; set; }
+
+        [Required, MinLength(3), MaxLength(16)]
+        public string RoleName { get; set; }
+    }
+}
