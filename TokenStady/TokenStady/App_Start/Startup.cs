1010524041c3acd2d7e25090c6b92ea0c6b15315
m_bubela <m_bubela@mifprojects.com>
2021-11-15 13:20:35 +0200

    Added empty project

---
+++ TokenStady/TokenStady/App_Start/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(TokenStady.App_Start.Startup))]
+namespace TokenStady.App_Start
+{
+    public class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthServerOptions { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            //DIConfig.Configure();
+            OwinConfig.Configuration(app);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //AzureStorageQueueConfig.Configure();
+            //FilterConfig.RegisterGlobalFilters();
+            OAuthConfigure(app);
+        }
+
+        public void OAuthConfigure(IAppBuilder app)
+        {
+            OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            app.UseOAuthBearerTokens(OAuthServerOptions);
+        }
+
+    }
+}
