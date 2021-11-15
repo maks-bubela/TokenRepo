d492cdcb6ce9d8d41d63c5fa299a8b2b13f41a60
m_bubela <m_bubela@mifprojects.com>
2021-11-15 13:21:21 +0200

    Added authentication contoller for send data to tokenprovider

---
+++ TokenStady/TokenStady/Controller/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using MintyIssueTracker.Models;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TokenStady.Controller
+{
+    [RoutePrefix("api/image")]
+    public class AuthenticationController : ApiController
+    {
+        public AuthenticationController()
+        {
+
+        }
+        // POST api/users/register
+        [HttpPost, Route("register")]
+        public async Task<HttpResponseMessage> RegisterAsync([FromBody] UserRegistrationModel userModel)
+        {
+            var result ="";
+            if (ModelState.IsValid)
+            {
+                result = TokenProvider.TokenProvider.GenerateToken(userModel);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadGateway, result);
+        }
+    }
+}
