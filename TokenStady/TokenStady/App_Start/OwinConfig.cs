841a76f8c04b86b1bcce4b15dbfde8e42faeeb4d
m_bubela <m_bubela@mifprojects.com>
2021-11-15 13:20:46 +0200

    Added owin config

---
+++ TokenStady/TokenStady/App_Start/OwinConfig.cs
@@ -0,0 +1,34 @@
+﻿using Owin;
+using System;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Infrastructure;
+using Microsoft.Owin.Security.Cookies;
+
+namespace TokenStady.App_Start
+{
+    public class OwinConfig
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(options: new CookieAuthenticationOptions
+            {
+                AuthenticationType = nameof(TokenStady),
+                AuthenticationMode = AuthenticationMode.Active,
+                CookieName = nameof(TokenStady),
+                CookieHttpOnly = true,
+                CookiePath = "/",
+                ExpireTimeSpan = TimeSpan.FromDays(7),
+                CookieManager = new CookieManager(),
+                LoginPath = new PathString("/Authentication/Login"),
+                Provider = new CookieAuthenticationProvider()
+                {
+                    OnResponseSignIn = ctx =>
+                    {
+                        ctx.Properties.IsPersistent = true;
+                    }
+                }
+            });
+        }
+    }
+}
